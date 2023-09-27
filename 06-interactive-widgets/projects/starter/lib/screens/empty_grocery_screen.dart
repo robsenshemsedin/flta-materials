c22c9e165eb040fa169b35220f1ca9fc5e74a7c1
robsenshemsedin <91184071+robsenshemsedin@users.noreply.github.com>
2023-09-27 06:17:57 +0300

    Adding layout widgets

--- 06-interactive-widgets/projects/starter/lib/screens/empty_grocery_screen.dart
+++ 06-interactive-widgets/projects/starter/lib/screens/empty_grocery_screen.dart
@@ -4,7 +4,20 @@ class EmptyGroceryScreen extends StatelessWidget {
   const EmptyGroceryScreen({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    // TODO 3: Replace and add layout widgets
-    return Container(color: Colors.purple);
+    return const Padding(
+        padding: EdgeInsets.all(30.0),
+        // 2
+        child: Center(
+          // 3
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // TODO 4: Add empty image
+              // TODO 5: Add empty screen title
+              // TODO 6: Add empty screen subtitle
+              // TODO 7: Add browse recipes button
+            ],
+          ),
+        ));
   }
 }
