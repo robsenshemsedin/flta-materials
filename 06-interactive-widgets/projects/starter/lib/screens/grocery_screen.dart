82437eb7f428b1089ea9aab1638a80a1d2aaa438
robsenshemsedin <91184071+robsenshemsedin@users.noreply.github.com>
2023-09-27 12:10:06 +0300

    Added grocery data consumer

--- 06-interactive-widgets/projects/starter/lib/screens/grocery_screen.dart
+++ 06-interactive-widgets/projects/starter/lib/screens/grocery_screen.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
-
+import 'package:provider/provider.dart';
+import '../models/models.dart';
 import 'empty_grocery_screen.dart';
 
 class GroceryScreen extends StatelessWidget {
   const GroceryScreen({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return const EmptyGroceryScreen();
+    return Consumer<GroceryManager>(builder: ((context, groceryManager, child) {
+      if (!groceryManager.groceryItems.isNotEmpty) {
+        return Container(
+          color: Colors.amberAccent,
+        );
+      } else {
+        return const EmptyGroceryScreen();
+      }
+    }));
   }
   // TODO: Add buildGroceryScreen
 }
