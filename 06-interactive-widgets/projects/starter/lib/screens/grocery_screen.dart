e81fd8afdb3adf0db0f6327ae0449a8bb118b9aa
robsenshemsedin <91184071+robsenshemsedin@users.noreply.github.com>
2023-09-27 13:49:38 +0300

    Presenting GroceryItemScreen

--- 06-interactive-widgets/projects/starter/lib/screens/grocery_screen.dart
+++ 06-interactive-widgets/projects/starter/lib/screens/grocery_screen.dart
@@ -2,15 +2,45 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../models/models.dart';
 import 'empty_grocery_screen.dart';
+import 'grocery_item_screen.dart';
 
 class GroceryScreen extends StatelessWidget {
   const GroceryScreen({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Consumer<GroceryManager>(builder: ((context, groceryManager, child) {
-      if (!groceryManager.groceryItems.isNotEmpty) {
-        return Container(
-          color: Colors.amberAccent,
+      if (groceryManager.groceryItems.isNotEmpty) {
+        return Scaffold(
+          // 6
+          floatingActionButton: FloatingActionButton(
+            child: const Icon(Icons.add),
+            onPressed: () {
+              final manager =
+                  Provider.of<GroceryManager>(context, listen: false);
+// 2
+              Navigator.push(
+                context,
+                // 3
+                MaterialPageRoute(
+                  // 4
+                  builder: (context) => GroceryItemScreen(
+                    // 5
+                    onCreate: (item) {
+                      // 6
+
+                      manager.addItem(item);
+                      // 7
+                      Navigator.pop(context);
+                    },
+                    // 8
+                    onUpdate: (item) {},
+                  ),
+                ),
+              );
+            },
+          ),
+          // 7
+// body: buildGroceryScreen(),
         );
       } else {
         return const EmptyGroceryScreen();
