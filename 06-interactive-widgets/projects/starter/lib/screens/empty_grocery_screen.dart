b77abcadbf51715ec82e2ebb93d2643d084ae477
robsenshemsedin <91184071+robsenshemsedin@users.noreply.github.com>
2023-09-27 07:25:22 +0300

    Adding visual pieces

--- 06-interactive-widgets/projects/starter/lib/screens/empty_grocery_screen.dart
+++ 06-interactive-widgets/projects/starter/lib/screens/empty_grocery_screen.dart
@@ -4,18 +4,46 @@ class EmptyGroceryScreen extends StatelessWidget {
   const EmptyGroceryScreen({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-        padding: EdgeInsets.all(30.0),
+    return Padding(
+        padding: const EdgeInsets.all(30.0),
         // 2
         child: Center(
           // 3
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              // TODO 4: Add empty image
-              // TODO 5: Add empty screen title
-              // TODO 6: Add empty screen subtitle
-              // TODO 7: Add browse recipes button
+              const Align(
+                alignment: Alignment.center,
+                child: AspectRatio(
+                  aspectRatio: 1 / 1,
+                  child: Image(
+                    image:
+                        AssetImage('assets/fooderlich_assets/empty_list.png'),
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ),
+              const Text(
+                'No Groceries',
+                style: TextStyle(fontSize: 21.0),
+              ),
+              const SizedBox(height: 16.0),
+              const Text(
+                'Shopping for ingredients?\n'
+                'Tap the + button to write them down!',
+                textAlign: TextAlign.center,
+              ),
+              MaterialButton(
+                textColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30.0),
+                ),
+                color: Colors.green,
+                onPressed: () {
+                  // TODO 8: Go to Recipes Tab
+                },
+                child: const Text('Browse Recipes'),
+              ),
             ],
           ),
         ));
