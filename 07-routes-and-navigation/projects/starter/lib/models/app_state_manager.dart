e454a4925804ad41ee222d8051d1b7ebc3a221b7
robsenshemsedin <91184071+robsenshemsedin@users.noreply.github.com>
2023-09-30 14:25:30 +0300

    Added the project

--- 07-routes-and-navigation/projects/starter/lib/models/app_state_manager.dart
+++ 07-routes-and-navigation/projects/starter/lib/models/app_state_manager.dart
@@ -67,3 +67,4 @@ class AppStateManager extends ChangeNotifier {
     notifyListeners();
   }
 }
+
--- 07-routes-and-navigation/projects/starter/lib/screens/splash_screen.dart
+++ 07-routes-and-navigation/projects/starter/lib/screens/splash_screen.dart
@@ -7,10 +7,19 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Provider.of<AppStateManager>(context, listen: false).initializeApp();
     return Center(
       child: Column(
         children: [Image.asset('assets/fooderlich_assets/rw_logo.png')],
       ),
+    );
+  }
+
+  static MaterialPage page() {
+    return const MaterialPage(
+      name: 'Splash',
+      key: ValueKey('/Splash'),
+      child: SplashScreen(),
     );
   }
 }
